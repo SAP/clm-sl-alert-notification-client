70408690a8019948e1a0e5016abdf6033835f107
Kristina Dzhurdzhina <kristina.dzhurdzhina@sap.com>
2019-12-09 17:41:05 +0200

    Update ServiceRegion.java

--- src/main/java/com/sap/cloud/alert/notification/client/ServiceRegion.java
+++ src/main/java/com/sap/cloud/alert/notification/client/ServiceRegion.java
@@ -18,6 +18,18 @@ public class ServiceRegion {
     public static final ServiceRegion US10 = new ServiceRegion(Platform.CF, "https://clm-sl-ans-live-ans-service-api.cfapps.us10.hana.ondemand.com/");
     public static final ServiceRegion JP10 = new ServiceRegion(Platform.CF, "https://clm-sl-ans-live-ans-service-api.cfapps.jp10.hana.ondemand.com/");
 
+    public static final ServiceRegion NEO_ROT = EU1;
+    public static final ServiceRegion NEO_FRANKFURT = EU2;
+    public static final ServiceRegion NEO_AMSTERDAM = EU3;
+    public static final ServiceRegion NEO_ASHBURN = US1;
+    public static final ServiceRegion NEO_CHANDLER = US2;
+    public static final ServiceRegion NEO_STERLING = US3;
+    public static final ServiceRegion NEO_COLORADO_SPRINGS = US4;
+    public static final ServiceRegion NEO_TOKYO = JP1;
+    public static final ServiceRegion CF_AWS_FRANKFURT = EU10;
+    public static final ServiceRegion CF_AWS_US_EAST = US10;
+    public static final ServiceRegion CF_AWS_TOKYO = JP10;
+
     private final Platform platform;
     private final URI serviceURI;
 

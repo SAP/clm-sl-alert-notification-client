3cb8aadfa1a7981c5f1daa906ab462c05f69fa60
Milko Kunev <milko.kunev@sap.com>
2020-12-02 12:38:11 +0200

    Add import export features (#17)
    * Add import export features
    
    * fix client test
    
    Co-authored-by: Simeon Petkov <simeon.petkov.96@gmail.com>

--- src/main/java/com/sap/cloud/alert/notification/client/IAlertNotificationConfigurationClient.java
+++ src/main/java/com/sap/cloud/alert/notification/client/IAlertNotificationConfigurationClient.java
@@ -1,5 +1,10 @@
 package com.sap.cloud.alert.notification.client;
 
+import com.sap.cloud.alert.notification.client.model.configuration.Configuration;
+
 public interface IAlertNotificationConfigurationClient extends IActionConfigurationClient, IConditionConfigurationClient, ISubscriptionConfigurationClient {
 
+    Configuration importConfiguration(Configuration newConfiguration);
+
+    Configuration exportConfiguration();
 }

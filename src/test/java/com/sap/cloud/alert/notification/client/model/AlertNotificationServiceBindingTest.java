898207039302781bb809a3715530d9fd81866c84
Paunov <ivan.paunov@sap.com>
2024-02-13 13:51:43 +0200

    Fix mtls from binding

--- src/test/java/com/sap/cloud/alert/notification/client/model/AlertNotificationServiceBindingTest.java
+++ src/test/java/com/sap/cloud/alert/notification/client/model/AlertNotificationServiceBindingTest.java
@@ -4,16 +4,18 @@ import io.pivotal.cfenv.core.CfCredentials;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
 
 import static com.sap.cloud.alert.notification.client.TestUtils.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class AlertNotificationServiceBindingTest {
 
     private static final String OAUTH_URL = "oauth_url";
+    private static final String TEST_CERTIFICATE = "TEST_CERTIFICATE";
+    private static final String TEST_PRIVATE_KEY = "TEST_PRIVATE_KEY";
 
     private Map<String, Object> credentials;
     private AlertNotificationServiceBinding classUnderTest;
@@ -24,6 +26,8 @@ public class AlertNotificationServiceBindingTest {
         credentials.put("client_id", TEST_USERNAME);
         credentials.put("client_secret", TEST_PASSWORD);
         credentials.put("url", TEST_SERVICE_URI.toString());
+        credentials.put("certificate", TEST_CERTIFICATE);
+        credentials.put("key", TEST_PRIVATE_KEY);
         credentials.put(OAUTH_URL, TEST_OAUTH_SERVICE_URI.toString());
 
         classUnderTest = new AlertNotificationServiceBinding(new CfCredentials(credentials));
@@ -50,11 +54,44 @@ public class AlertNotificationServiceBindingTest {
     }
 
     @Test
+    public void whenGetCertificateSecretIsCalled_thenCorrectValueIsReturned() {
+        assertEquals(classUnderTest.getCertificate(), TEST_CERTIFICATE);
+    }
+
+    @Test
+    public void whenGetPrivateKeySecretIsCalled_thenCorrectValueIsReturned() {
+        assertEquals(classUnderTest.getPrivateKey(), TEST_PRIVATE_KEY);
+    }
+
+    @Test
     public void givenBindingOfTypeBasic_whenCreatingServiceBinding_thenCorrectServiceBindingIsReturned() {
         credentials.remove(OAUTH_URL);
 
         assertNull(new AlertNotificationServiceBinding(new CfCredentials(credentials)).getOauthUri());
     }
 
+    @Test
+    public void givenOAuthCredentials_whenCreatingServiceBinding_thenCorrectServiceBindingIsReturned() {
+        AlertNotificationServiceBinding createdServiceBinding = new AlertNotificationServiceBinding(TEST_SERVICE_URI, URI.create(OAUTH_URL), TEST_USERNAME, TEST_PASSWORD);
+
+        assertEquals(createdServiceBinding.getServiceUri(), TEST_SERVICE_URI);
+        assertEquals(createdServiceBinding.getOauthUri(), URI.create(OAUTH_URL));
+        assertEquals(createdServiceBinding.getClientId(), TEST_USERNAME);
+        assertEquals(createdServiceBinding.getClientSecret(), TEST_PASSWORD);
+        assertNull(createdServiceBinding.getCertificate());
+        assertNull(createdServiceBinding.getPrivateKey());
+    }
+
+    @Test
+    public void givenOAuthWithCertificateCredentials_whenCreatingServiceBinding_thenCorrectServiceBindingIsReturned() {
+        AlertNotificationServiceBinding createdServiceBinding = new AlertNotificationServiceBinding(TEST_SERVICE_URI, URI.create(OAUTH_URL), TEST_USERNAME, TEST_CERTIFICATE, TEST_PRIVATE_KEY);
+
+        assertEquals(createdServiceBinding.getServiceUri(), TEST_SERVICE_URI);
+        assertEquals(createdServiceBinding.getOauthUri(), URI.create(OAUTH_URL));
+        assertEquals(createdServiceBinding.getClientId(), TEST_USERNAME);
+        assertEquals(createdServiceBinding.getCertificate(), TEST_CERTIFICATE);
+        assertEquals(createdServiceBinding.getPrivateKey(), TEST_PRIVATE_KEY);
+        assertNull(createdServiceBinding.getClientSecret());
+    }
 }
 
